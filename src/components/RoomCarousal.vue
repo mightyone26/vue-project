efa1c18a9ae7d6952a0d839ed40ea53fe0eeb0af
mightyone26 <dkduplessis@hotmail.com>
2022-04-20 06:41:29 +1000

    commit 3

--- src/components/RoomCarousal.vue
+++ src/components/RoomCarousal.vue
@@ -1,65 +1,47 @@
 <template>
-  <div class="row">
-  <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
+<div v-for="room in room" :key="room.id">
+    
+ 
+    <div class="card" style="width: 18rem;" >
+    <img src="{{room.pic}}" class="card-img-top" alt="room pic">
+    <div class="card-body"  >
+        <p class="card-text">{{room.description}}</p>
     </div>
-  </div>
-  
-  <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere e</a>
-      </div>
     </div>
-  </div>
 </div>
-<div class="row">
-  <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere e</a>
-      </div>
-    </div>
-  </div>
-</div>
- <div class="row">
-  <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere e</a>
-      </div>
-    </div>
-  </div>
-</div>
+<div>Room carousal component</div>
+ 
 
 </template>
 
+<script setup>
+
+    
+
+    const room = [{
+        id: "1",
+        description: "Southern Rooms",
+        pic: "@/assets/RoomPictures/pexels-asad-photo-maldives-1268871.jpg"
+    },
+    {
+        id: "2",
+        description: "Nothern Rooms",
+        pic: "@/assets/RoomPictures/pexels-asad-photo-maldives-1450363.jpg"
+    },
+    {
+        id: "3",
+        description: "Western Rooms",
+        pic: "@/assets/RoomPictures/pexels-leonardo-rossatti-2598638.jpg"
+    },
+    {
+        id: "4",
+        description: "Eastern Rooms",
+        pic: "@/src/assets/RoomPictures/pexels-vincent-gerbouin-1179156.jpg"
+    },
+    ];
+    
+
+
+
+
+</script>
