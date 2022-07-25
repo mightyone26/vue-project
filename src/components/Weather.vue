c8cacf95be13182e0bb0effd5e2d340d0ab828d7
mightyone26 <dkduplessis@hotmail.com>
2022-07-26 03:24:37 +1000

    26july

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="myCard">
-    <h1>Weather</h1>
+    <h1>Live Weather</h1>
      
      <!-- <div  v-for="axGet in axGet" :key="axGet.id">  
-      <h4><b>{{axGet.first_name}}</b></h4>
-      <h4>{{axGet.email}}</h4>
-      <img :src="axGet.avatar" >
-     </div> -->
+      <h4>{{axGet.name}}</h4>      
+      <h4>{{axGet.main}}</h4> 
+      <h4>{{ axGet.temp }}</h4>
+      <h4>{{axGet.temp_min}}</h4> 
+      <h4>{{axGet.humidity}}</h4>  -->
+    <h4>Temperature {{temp}}dergree celisus</h4>
+    <h4>Wind {{wind}}km</h4>
+      
+
+      <!-- <img :src="axGet.avatar" > -->
+     <!-- </div> -->
   
   </div>
 </template>
@@ -16,14 +23,17 @@
 import axios from 'axios'
 import { ref } from 'vue' 
 
-const axGet = ref()
-//748ad21f2c5cfd54fdf76912d8aaa9e1
-axios.get('https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={748ad21f2c5cfd54fdf76912d8aaa9e1}').then((res) => {        
-        const zxc = res.data     
-        axGet.value = zxc.data
-        console.log('weather', res)
+const temp = ref()
+const wind = ref()
+
+
+axios.get('https://api.openweathermap.org/data/2.5/weather?lat=-35.2821&lon=174.0910&units=metric&appid=748ad21f2c5cfd54fdf76912d8aaa9e1').then((res) => {        
+       
+        temp.value = res.data.main.temp
+        wind.value = res.data.wind.speed
+        console.log('weather', res.data)
       });
-     
+
 
 </script>
 
